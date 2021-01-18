d05bf3ba09b166230ee2cdcf1f83696034a0c568
Hyung-Taik Choi <htcrefactor@ajou.ac.kr>
2021-01-18 13:12:30 +0900

    Square calls this.props.onClick when clicked

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class Square extends React.Component {
     render() {
         return (
             <button className="square" 
-                    onClick={() => this.setState({value: 'X'})} >
+                    onClick={() => this.props.onClick()} >
                 {this.props.value}
             </button>
         );
